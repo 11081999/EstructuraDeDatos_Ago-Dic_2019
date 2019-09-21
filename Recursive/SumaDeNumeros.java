9587bb7415a33ac5f0c8061f7e7d8211e231de98
11081999 <robertoriverateran@gmail.com>
2019-09-21 16:06:16 -0500

    2

--- Recursive/SumaDeNumeros.java
+++ Recursive/SumaDeNumeros.java
@@ -1,33 +1,13 @@
 public class SumaDeNumeros {
-  private int resultado= 0;
-
-  public static sumaDeNumeros(int numeroMaximo){
-    this.resultado= resutlado;
-
-    for (int i; i <= numeroMaximo; i++){
-      resultado+=1;
-
-    }
-    return resultado;
-
-  }
-
-  public int getResultado(){
-    return resultado;
-  }
-
-  public int setResultado(int i){
-    return this.resultado= i;
-  }
 
   public static int sumaDeNumerosRecursiva(int numeroMaximo){
     if (numeroMaximo==1){
       return 1;
 
     }else {
-      return numeroMaximo+ sumaDeNumerosRecursiva(numeroMaximo);
+      return numeroMaximo+ sumaDeNumerosRecursiva(numeroMaximo-1);
 
     }
   }
 
-}
+} 
