e363754aa163634071f7b6ea39b833015a9f5fac
11081999 <robertoriverateran@gmail.com>
2019-09-21 19:03:38 -0500

    Agregar metodos

--- Recursive/Fibonacci.java
+++ Recursive/Fibonacci.java
@@ -1,18 +1,6 @@
 public class Fibonacci{
-  private int a= 0;
-  private int b= 1;
-  private int sum= 0;
-
-  public static Fibonacci(int a, int b){
-    for(int i = 2; i < number; i++){
-      sum= a + b;
-      a= b;
-      b= sum;
-    }
-
-  }
-
-  private static int fibonacci(int number){
+ 
+  public static int fibonacci(int number){
       if(number==0){
         return 0;
       }
