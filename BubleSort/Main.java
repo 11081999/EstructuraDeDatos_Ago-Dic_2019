63f1b65b913950b29410f4bdec84f2a88e819eab
Roberto Rivera Terán <33244698+11081999@users.noreply.github.com>
2019-09-27 12:09:42 -0500

    save

--- BubleSort/Main.java
+++ BubleSort/Main.java
@@ -2,10 +2,18 @@ public class Main {
 
 	public static void main(String args[]) {
 
+		//hacerlo con strings
+		/*
     Alumno t1<String> = new ListaLigada<>;
     Alumno t2<String> = new ListaLigada<>;
+		t1.CompareTo(t2);
+		*/
 
-    t1.CompareTo(t2);
+		//hacerlo con ints
+		int 
+
+
+
 
   }
 
