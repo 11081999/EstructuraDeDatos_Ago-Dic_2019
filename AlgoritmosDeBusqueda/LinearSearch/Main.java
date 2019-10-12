7049ea67076083d9c5c199d09240a38bb65ccafa
Roberto Rivera Terán <33244698+11081999@users.noreply.github.com>
2019-10-12 14:51:41 -0500

    fixxes

--- AlgoritmosDeBusqueda/LinearSearch/Main.java
+++ AlgoritmosDeBusqueda/LinearSearch/Main.java
@@ -7,11 +7,12 @@ public class Main {
 		lista.insertarAlUltimo(30);
 		lista.insertarAlUltimo(35);
 		lista.imprimeLista();
-		
-		System.out.println("El elemento en la pos 1 es: " + lista.encontrarElementoEnElIndice(1);
+
+		System.out.println("El elemento en la pos 0 es: " + lista.encontrarElementoEnElIndice(0));
+		System.out.println("El elemento en la pos 1 es: " + lista.encontrarElementoEnElIndice(1));
 		//Alterantivamente se puede usar
 		System.out.println("El elemento en la pos 2 es (lin): " + lista.encontrarNodoEnIndice(2).getElemento());
 		//VS Binary Search
-		System.out.println("El elemento 30 esta en la pos (bin): " + lista.encontrarNodoEnIndice(30));
+		System.out.println("El elemento 30 esta en la pos (bin): " + lista.busquedaBinaria(30));
 	}
 }
