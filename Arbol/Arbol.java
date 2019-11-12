4f266ebb807a35fe92b5fbe1040e22f5b658dd46
Roberto Rivera Terán <33244698+11081999@users.noreply.github.com>
2019-11-12 11:52:28 -0600

    f

--- Arbol/Arbol.java
+++ Arbol/Arbol.java
@@ -1,10 +1,25 @@
 
-public class ArbolBinario {
+public class Arbol<T extendas comparable<T>> {
 
-	private K llave;
-	private V valor;
+	private Nodo<T> raiz;
 
 	public Entrada() {
+
+	}
+
+	public Nodo<T> getRaiz(){
+		return raiz;
+	}
+
+	public void setRaiz(Nodo<T> raiz){
+		this.raiz= raiz;
+	}
+
+	public void insertar(T elemento){
+		if (raiz==null){
+			raiz= new Nodo<>(elemento);
+		}
+
 
 	}
 
